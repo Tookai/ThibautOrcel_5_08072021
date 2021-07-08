693625ad748ad660903fc78437b2a9ccfeb6e377
Thibaut <tybo.orcel@gmail.com>
2021-07-08 19:31:11 +0200

    display item in cart

--- public/javascript/cart.js
+++ public/javascript/cart.js
@@ -1,3 +1,7 @@
+window.onload = () => {
+  teddiesInCart();
+};
+
 if (localStorage.length == 0) {
   console.log("le storage est vide");
   document.getElementById("totalPriceTag").innerText = `Votre Panier est vide ! Retournez à la page d'accueil pour commencer vos achats.`;
@@ -10,4 +14,26 @@ document.getElementById("clear").onclick = () => {
     window.localStorage.clear();
     window.location.reload();
   }
+};
+
+const cart = JSON.parse(localStorage.getItem("cart"));
+
+console.log(cart);
+
+teddiesInCart = () => {
+  cart.map((teddyItem) => {
+    //
+    // get & clone template
+    const template = document.querySelector("template#cart-item");
+    const productContainer = document.importNode(template.content, true);
+
+    // fill template
+    productContainer.querySelector(".tedImage").setAttribute("src", teddyItem.img);
+    productContainer.querySelector(".tedImage").setAttribute("alt", `Photo de l'ourson : ${teddyItem.nom}`);
+    productContainer.querySelector(".tedName").textContent = teddyItem.nom;
+    productContainer.querySelector(".tedPrice").textContent = teddyItem.prix;
+
+    // render template
+    document.getElementById("cart").appendChild(productContainer);
+  });
 };
